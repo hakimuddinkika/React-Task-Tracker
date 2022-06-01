b74308ccddaf47e6f6d6cd036bc8013dd67c2aab
Hakimuddin Kika <Hakimuddin Kika>
2022-06-01 15:42:21 +0300

    Installing React Icons, Separating Components, Deleting Tasks from the list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-  const name = 'Hakimuddin';
-  const x = true;
+	// const name = 'Hakimuddin';
+	// const x = true;
 
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+	const [tasks, setTasks] = useState([
+		{
+			id : 1,
+			text : 'Doctors Appointment',
+			day : 'Feb 5th at 2:30am',
+			reminder : true,
+		},
+		{
+			id : 2,
+			text : 'Meeting at School',
+			day : 'Feb 6th at 1:30am',
+			reminder : true,
+		},
+		{
+			id : 3,
+			text : 'Food Shopping',
+			day : 'Feb 7th at 3:30am',
+			reminder : false,
+		},
+	])
+
+	//Delete Task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id))
+	}
+
+	return (
+		<div className="container">
+			<Header />
+			{ tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask}/>
+			) : (
+				'No tasks to show'
+			)
+			}
+		</div>
+	);
 }
+
+
 
 export default App;
