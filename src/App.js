dfac8021b01d856eea06589adc52d79b88ba65fc
Hakimuddin Kika <Hakimuddin Kika>
2022-06-06 16:56:42 +0300

    Implement React Router DOM v6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react"
-import AddTask from "./components/AddTask";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import AddTask from "./components/AddTask"
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import Footer from "./components/Footer"
+import About from "./components/About"
+
 
 function App() {
 	// const name = 'Hakimuddin';
@@ -85,19 +89,33 @@ function App() {
 	}
 
 	return (
-		<div className="container">
-			<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-			{showAddTask && <AddTask onAdd={addTask}/>}
-			{ tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-			) : (
-				'No tasks to show'
-			)
-			}
-		</div>
+		<Router>
+			<div className="container">
+				<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+				
+				<Routes>
+					<Route 
+						path='/' 
+						 
+						element={
+							<>
+								{showAddTask && <AddTask onAdd={addTask}/>}
+								{ tasks.length > 0 ? (
+									<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+								) : (
+									'No tasks to show'
+								)
+								}
+							</>
+						}
+					/>
+					<Route path='/about' element={<About />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
-
-
 export default App;
+
